e75ec37e45776f12d6ba9fbc9c3a7280856fba55
Ana Thiel Fortes <anathiel29@hotmail.com>
2024-09-09 17:40:52 -0300

    exercicio

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log("hello, world")
+
+let numero1 = 6;
+let numero2 = 8;
+let numero3 = 7;
+let soma = numero1 + numero2 + numero3;
+let media = soma / 3;
+console.log( "A soma média dos números é: " + media);
+
