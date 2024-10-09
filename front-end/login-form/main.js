7e5b12c2f66c69e512e36aab96d4c6af860734dc
ANA PAULA CAMPOS THIEL FORTES <01693365065@senacrs.edu.br>
2024-10-09 17:23:21 -0300

    formulario

--- front-end/login-form/main.js
+++ front-end/login-form/main.js
@@ -63,3 +63,34 @@ function validatedEmail(email) {
   console.log(customType);
 }
  */
+
+document
+  .getElementById("loginForm")
+  .addEventListener("submit", async function (event) {
+    event.preventDefault();
+
+    const username = document.getElementById("name").value;
+    const password = document.getElementById("password").value;
+
+    const responseElement = document.getElementById("response");
+    responseElement.innerHTML = "";
+
+    try {
+      const response = await fetch("https://dummyjson.com/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+      });
+
+      const data = await response.json();
+      if (response.ok) {
+        responseElement.innerHTML = `<p>Login bem sucedido! Token &{data.acessToken</p>`;
+      } else {
+        responseElement.innerHTML = `<p>Erro: &{data.message</p>`;
+      }
+    } catch (error) {
+      responseElement.innerHTML = `<p>Erro na requisição: &{error.message</p>`;
+    }
+  });
